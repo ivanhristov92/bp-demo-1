b141f7077b9f6f8a9e18043d5054a81240066ed5
Ivan Hristov <ivan.hristov.profile@gmail.com>
2025-04-13 15:07:35 +0100

    updates

--- src/app/page-viewer/page.tsx
+++ src/app/page-viewer/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <PageViewerPresenter
       defaultComparisonImage={
-        "https://ihac-academy-assets.s3.us-east-1.amazonaws.com/Website+View.png"
+        "https://ihac-academy-assets.s3.us-east-1.amazonaws.com/ihac-academy-demo-1/Group+13.png"
       }
       defaultHostedWebsiteUrl={"http://localhost:3000"}
     />
